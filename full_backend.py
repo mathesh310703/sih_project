bc58be61842d1325b6de246140f62c4fa1af943f
Matheshwaran Prakash <121600790+mathesh310703@users.noreply.github.com>
2023-12-20 11:57:13 +0530

    Update full_backend.py

--- full_backend.py
+++ full_backend.py
@@ -1,16 +1,13 @@
 import  psycopg2
-connection = psycopg2.connect(host = "localhost",port = "5432",database = "dbhost",user = "postgres",password = "root")
+connection = psycopg2.connect(host = "localhost",port = "5432",database = "localhost",user = "postgres",password = "root")
 
 cursor = connection.cursor()
 print("database is connected successfully")
 
 disease = input()
-if disease == "apraxia":
-    cursor.execute("select therapy_text from therapy where t_id = 1;")
-elif disease == "dysrthria":
-    cursor.execute("select therapy_text from therapy where t_id = 2;")
-elif disease == "stuttering":
-    cursor.execute("select therapy_text from therapy where t_id = 3;")
+if disease == "articulation disorder":
+    cursor.execute("select therapy_words from therapy where p_id = 1001;")
+
 
 a = cursor.fetchall()
 
@@ -22,11 +19,9 @@ result = ''.join(lst)
 retrived_therapy = (result[2:-3])
 print(retrived_therapy)
 
-import pyttsx3 
+
 instruction = retrived_therapy
-text_speech = pyttsx3.init()
-text_speech.say(instruction)
-text_speech.runAndWait()
+
 
 import speech_recognition as sr
 r = sr.Recognizer()
@@ -39,6 +34,7 @@ try:
 except Exception as e:
      print('Sorry, I could not convert the audio to text')
 
+text = text.lower()
 if text == instruction:
     print("your exercise is successfully completed")
 else:
